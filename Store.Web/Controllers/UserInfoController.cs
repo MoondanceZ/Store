274963fa513301cc9d1054d5556e78808c5e59bb
MoondanceZ <QYK_zZ@hotmail.com>
2017-08-18 10:56:04 +0800

    update

--- Store.Web/Controllers/UserInfoController.cs
+++ Store.Web/Controllers/UserInfoController.cs
@@ -1,4 +1,6 @@
-﻿using Store.Repository.Interface;
+﻿using Store.Model;
+using Store.Repository;
+using Store.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +12,29 @@ namespace Store.Web.Controllers
     public class UserInfoController : Controller
     {
         IUserInfoRepository _userInfoRepository;
-        public UserInfoController(IUserInfoRepository userInfoRepository)
+        IUnitOfWork _unitOfWork;
+        public UserInfoController(IUserInfoRepository userInfoRepository, IUnitOfWork unitOfWork)
         {
             _userInfoRepository = userInfoRepository;
+            _unitOfWork = unitOfWork;
         }
         // GET: UserInfo
         public ActionResult Index()
         {
-            var a = _userInfoRepository;
-            var b = a.GetAll();
+
+            List<UserInfo> list = new List<UserInfo>();
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(new UserInfo
+                {
+                    Account = "account" + i
+                });
+            }
+            ViewBag.St = DateTime.Now.ToString();
+            // var a = _userInfoRepository;
+            //a.AddAll(list);
+            // _unitOfWork.Commit();
+            ViewBag.Et = DateTime.Now.ToString();
             return View();
         }
     }
